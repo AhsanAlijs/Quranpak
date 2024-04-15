7b74f89c40ff41e0376680b26898a5c94a4832da
ahsanalijs <ahsanali45444@gmail.com>
2024-04-16 01:05:22 +0500

    grab elements in obj

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const compSurah = document.querySelector('.compSurah');
 
 const data = async () => {
     try {
-        const ayah = await axios.get(`https://equran.id/api/v2/surat/1`);
+        const ayah = await axios.get(`https://equran.id/api/v2/surat`);
         const ayat = ayah.data.data.ayat;
-        ayat.forEach(element => {
-            console.log(element);
-            surrahName.innerHTML = `${ayah.data.data.nama}`;
-            compSurah.innerHTML += `<p class='ayano'>${element.nomorAyat} </p>${element.teksArab}`;
+        const name = ayah.data.data[0].nama;
+        const tempatTurun = ayah.data.data[0].tempatTurun;
+        const namaLatin = ayah.data.data[0].namaLatin;
+        const nomor = ayah.data.data[0].nomor;
+        console.log(name);
+        console.log(nomor);
+        console.log(tempatTurun);
+        console.log(namaLatin);
 
-        });
+        // ayat.forEach(element => {
+        //     console.log(element);
+        //     surrahName.innerHTML = `${ayah.data.data.nama}`;
+        //     compSurah.innerHTML += `<p class='ayano'>${element.nomorAyat} </p>${element.teksArab}`;
+
+        // });
     } catch (error) {
         console.log(error);
     }
